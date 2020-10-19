82573ad63e5d60af8cf64eb9dd44fe1baf7008ba
Keony Schröer <keony@edu.univali.br>
2020-10-19 19:55:13 -0300

    chore: add new properties to topics document

--- go/firebase.go
+++ go/firebase.go
@@ -3,7 +3,7 @@ package main
 import (
 	"context"
 	"log"
-
+	"time"
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
 	"google.golang.org/api/option"
@@ -32,8 +32,10 @@ func init() {
 func addTopic(topic, payload string) {
 	ctx := context.Background()
 
-	_, _, err := client.Collection(topic).Add(ctx, map[string]interface{}{
+	_, _, err := client.Collection("topics").Add(ctx, map[string]interface{}{
 		"payload": payload,
+		"description": topic,
+		"insertedAt": time.Now(),
 	})
 
 	if err != nil {
